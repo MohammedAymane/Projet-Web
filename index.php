85f6f0b0599cc00c5dcad5263842f8a268267491
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2022-03-11 22:16:09 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,195 @@
+<div class="div"></div>
+<div class="div-2">Login</div>
+<div class="div-3"></div>
+<div class="div-4">Signup</div>
+<div class="div-5"></div>
+<div class="div-6">User Name</div>
+<div class="div-7"></div>
+<div class="div-8">Password</div>
+<div class="div-9"></div>
+<div class="div-10">Forgot Password?</div>
+<div class="div-11">Login</div>
+<div class="div-12">
+    <div class="div-13"></div>
+</div>
+<div class="div-14">Hash Tech</div>
+<div class="div-15">Powered By C# Ui Academy</div>
+<div class="icons-8-phoenix-framework-125-px">
+    <div class="builder-image-sizer image-sizer"></div>
+</div>
+<style>
+.div {
+    display: flex;
+    max-width: 87px;
+    height: 26px;
+    width: 87px;
+    border-radius: 20px;
+    border-color: rgba(242, 242, 242, 1);
+    border-width: 0px;
+    border-style: solid;
+    background-color: rgba(48, 27, 82, 1);
+}
+
+.div-2 {
+    max-width: 38px;
+    color: rgba(255, 255, 255, 1);
+    font-size: 12px;
+    letter-spacing: 0%;
+    text-align: left;
+    font-family: Roboto, sans-serif;
+}
+
+.div-3 {
+    display: flex;
+    max-width: 87px;
+    height: 26px;
+    width: 87px;
+    border-radius: 20px;
+    border-color: rgba(235, 87, 87, 1);
+    border-width: 0px;
+    border-style: solid;
+    background-color: rgba(219, 9, 22, 1);
+}
+
+.div-4 {
+    max-width: 38px;
+    color: rgba(255, 255, 255, 1);
+    font-size: 12px;
+    letter-spacing: 0%;
+    text-align: left;
+    font-family: Roboto, sans-serif;
+}
+
+.div-5 {
+    display: flex;
+    max-width: 190px;
+    height: 30px;
+    width: 190px;
+    border-radius: 20px;
+    border-color: rgba(130, 130, 130, 1);
+    border-width: 1px;
+    border-style: solid;
+    background-color: rgba(246, 246, 246, 1);
+}
+
+.div-6 {
+    max-width: 156px;
+    color: rgba(171, 170, 170, 1);
+    font-size: 12px;
+    letter-spacing: 0%;
+    text-align: left;
+    font-family: Roboto, sans-serif;
+}
+
+.div-7 {
+    display: flex;
+    max-width: 190px;
+    height: 30px;
+    width: 190px;
+    border-radius: 20px;
+    border-color: rgba(130, 130, 130, 1);
+    border-width: 1px;
+    border-style: solid;
+    background-color: rgba(246, 246, 246, 1);
+}
+
+.div-8 {
+    max-width: 156px;
+    color: rgba(168, 166, 166, 1);
+    font-size: 12px;
+    letter-spacing: 0%;
+    text-align: left;
+    font-family: Roboto, sans-serif;
+}
+
+.div-9 {
+    display: flex;
+    max-width: 240px;
+    height: 235px;
+    width: 240px;
+    border-radius: 20px;
+    border-color: rgba(189, 189, 189, 1);
+    border-width: 2px;
+    border-style: solid;
+    background-color: rgba(244, 243, 243, 1);
+}
+
+.div-10 {
+    max-width: 156px;
+    color: rgba(48, 27, 82, 1);
+    font-size: 12px;
+    letter-spacing: 0%;
+    text-align: left;
+    font-family: Roboto, sans-serif;
+}
+
+.div-11 {
+    max-width: 69px;
+    color: rgba(48, 27, 82, 1);
+    font-size: 24px;
+    letter-spacing: 0%;
+    text-align: left;
+    font-family: Roboto, sans-serif;
+}
+
+.div-12 {
+    display: flex;
+    flex-direction: column;
+    max-width: 727px;
+}
+
+.div-13 {
+    display: flex;
+    max-width: 727px;
+    height: 395px;
+    width: 727px;
+    border-radius: 20px;
+    border-color: rgba(242, 242, 242, 1);
+    border-width: 1px;
+    border-style: solid;
+    background-color: rgba(48, 27, 82, 1);
+}
+
+.div-14 {
+    max-width: 119px;
+    color: rgba(255, 255, 255, 1);
+    font-size: 24px;
+    letter-spacing: 0%;
+    text-align: left;
+    font-family: Roboto, sans-serif;
+}
+
+.div-15 {
+    max-width: 164px;
+    color: rgba(255, 255, 255, 1);
+    font-size: 12px;
+    letter-spacing: 0%;
+    text-align: left;
+    font-family: Roboto, sans-serif;
+}
+
+.icons-8-phoenix-framework-125-px {
+    display: flex;
+    position: relative;
+    min-width: 20px;
+    min-height: 20px;
+    max-width: 129px;
+}
+
+.image {
+    object-fit: cover;
+    object-position: center;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+}
+
+.image-sizer {
+    width: 100%;
+    padding-top: 63.565891472868216%;
+    pointer-events: none;
+    font-size: 0;
+}
+</style>
