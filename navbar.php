1a1317044d59935b10c5ef95bdf2303ee43b702d
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2022-03-13 17:39:12 +0100

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,44 @@
+<html lang="en">
+<link type="text/css" rel="stylesheet" id="dark-mode-custom-link">
+<link type="text/css" rel="stylesheet" id="dark-mode-general-link">
+<style lang="en" type="text/css" id="dark-mode-custom-style"></style>
+<style lang="en" type="text/css" id="dark-mode-native-style"></style>
+
+<head>
+    <link type="text/css" rel="stylesheet" id="dark-mode-custom-link">
+    <link type="text/css" rel="stylesheet" id="dark-mode-general-link">
+    <style lang="en" type="text/css" id="dark-mode-custom-style"></style>
+    <style lang="en" type="text/css" id="dark-mode-native-style"></style>
+
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <nav class="container-fluid navbar-dark bg-dark">
+
+
+
+        <div class=" navbar-dark row" id="navbarColor01">
+            <a class="m-2 col-1 navbar-brand" href="#">Logo</a>
+
+            <a class="col-2"><button class="m-2 btn btn-primary" href="#">Accueil</button></a><a class="col-2"><button
+                    class="m-2 btn btn-primary" href="#">Accueil</button></a>
+
+
+        </div>
+    </nav>
+
+
+
+
+
+</body>
+
+</html>
