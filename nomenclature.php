a0b90027cd5f32ad144a7073f5fe5b97a11b8b21
Novmbrain <fuwenjiexidian@gmail.com>
2022-03-18 17:08:11 +0100

    nomenclature add search bar

--- nomenclature.php
+++ nomenclature.php
@@ -23,28 +23,32 @@ $json = $nomenclature["result"];
 <div class="ui container">
   <div class="ui top attached segment">
     <div class="ui stackable secondary four item menu">
-      <button class="ui labeled icon button">
-        <i class="pause icon"></i>
+
+      <button class="ui labeled icon button" onclick="tree_create();">
+        <i class="file icon"></i>
         Create
       </button>
 
-      <button class="ui labeled icon button">
-        <i class="right arrow icon"></i>
+      <button class="ui labeled icon button" onclick="tree_rename();">
+        <i class="right undo icon"></i>
         Rename
       </button>
 
-      <button class="ui labeled icon button">
-        <i class="right arrow icon"></i>
+      <button class="ui labeled icon button" onclick="tree_delete();">
+        <i class="right trash alternate icon"></i>
         Delete
       </button>
 
-      <div class="ui search">
-        <div class="ui icon input">
-          <input class="prompt" type="text" placeholder="Common passwords...">
-          <i class="search icon"></i>
+      <div class="right menu">
+        <div class="ui search">
+          <div class="ui icon input">
+            <input class="prompt" type="text" id="search_bar" placeholder="Common passwords...">
+            <i class="search icon"></i>
+          </div>
+          <div class="results"></div>
         </div>
-        <div class="results"></div>
       </div>
+
     </div>
     
   </div>
@@ -72,17 +76,67 @@ $json = $nomenclature["result"];
 <!-- 5 include the minified jstree source -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.2.1/jstree.min.js"></script>
 <script>
+
+  function tree_create() {
+    var ref = $('#jstree').jstree(true),
+      sel = ref.get_selected();
+    if(!sel.length) { return false; }
+    sel = sel[0];
+    // sel = ref.create_node(sel, {"type":"file"});
+    sel = ref.create_node(sel);
+
+    if(sel) {
+      ref.edit(sel);
+    }
+  };
+
+  function tree_rename() {
+    var ref = $('#jstree').jstree(true),
+      sel = ref.get_selected();
+    if(!sel.length) { return false; }
+    sel = sel[0];
+    ref.edit(sel);
+  };
+
+  function tree_delete() {
+    var ref = $('#jstree').jstree(true),
+      sel = ref.get_selected();
+    if(!sel.length) { return false; }
+    ref.delete_node(sel);
+  };
+
+  // 7 bind to events triggered on the tree
+  function post(action, data) {
+    let request = new XMLHttpRequest();   
+    request.open('post', 'nomenclatureHandler.php');
+    request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    request.send("id="+data.node.id+"&parent="+data.node.parent+"&text="+data.node.text+"&action="+action);
+    // alert(action + " successfully");
+  }
+
   $(function () {
-    // 6 create an instance when the DOM is ready
+
+    // search code 
+    var to = false;
+    $('#search_bar').keyup(function () {
+      if(to) { clearTimeout(to); }
+      to = setTimeout(function () {
+        var v = $('#search_bar').val();
+        $('#jstree').jstree(true).search(v);
+      }, 250);
+    });
+
+    //  create an instance when the DOM is ready
     $("#jstree").jstree({
       "core" : {
         "multiple" : false,
         "animation" : 0,
+        "expand_selected_onload" : true,
         "themes" : {
           "variant" : "large",
-          "expand_selected_onload" : true,
           "stripes" : true 
         },
+
         "check_callback" : function (op, node, par, pos, more) {
                               if(op === "delete_node") {
                                   if (node.parent === '#') {
@@ -93,45 +147,31 @@ $json = $nomenclature["result"];
                           },
         "data" :  <?php echo "$json";?>
       },
-      "plugins" : [ "contextmenu","unique", "search"]
+      "plugins" : [ "unique", "search"]
     });
 
-    var to = false;
-    $('#jstree').keyup(function () {
-      if(to) { clearTimeout(to); }
-      to = setTimeout(function () {
-        var v = $('#jstree').val();
-        $('#jstree').jstree(true).search(v);
-      }, 250);
-    });
-
-    // 7 bind to events triggered on the tree
-    function post(action, data) {
-      let request = new XMLHttpRequest();   
-      request.open('post', 'nomenclatureHandler.php');
-      request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-      request.send("id="+data.node.id+"&parent="+data.node.parent+"&text="+data.node.text+"&action="+action);
-      // alert(action + " successfully");
-    }
-
+    //rename call back function
     $('#jstree').on("rename_node.jstree", function(e, data) {
       post("update", data);
-    })
-    
-    $('#jstree').on("create_node.jstree", function(e, data) {
-      post("create", data);
     });
 
+    //create call back function
+    $('#jstree').on("create_node.jstree", function(e, data) {
+      post("create", data);
+    }); 
+
+    //delete call back function
     $('#jstree').on("delete_node.jstree", function(e, data) {
       post("delete", data);
     });
 
-    // 8 interact with the tree - either way is OK
+    // interact with the tree - either way is OK
     $('button').on('click', function () {
       $('#jstree').jstree(true).select_node('child_node_1');
       $('#jstree').jstree('select_node', 'child_node_1');
       $.jstree.reference('#jstree').select_node('child_node_1');
     });
+
   });
 </script>
 </body>
