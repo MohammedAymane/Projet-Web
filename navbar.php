5a6b402c596e7a92d3bc5807b65ec09becb87687
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2022-03-20 12:55:23 +0100

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -54,22 +54,6 @@
                         <a class="nav-link active" aria-current="page" href="nomenclature.php">Nomenclature</a>
                     </li>';
                     } ?>
-
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
                 </ul>
                 <div class="d-flex">
                     <a href="logout.php" class="btn btn-danger" type="submit">Se deconnecter</a>
